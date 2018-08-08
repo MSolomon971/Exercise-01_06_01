33457f8171cb1ed26e2435fc46c5969d65bf0d05
Unknown <MSolomon971@west-mec.org>
2018-08-07 18:27:54 -0700

    Update
    Added validation for leap year

--- snoot.js
+++ snoot.js
@@ -10,15 +10,60 @@
 
 "use strict";
 
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
 // functions to turn off select list defaults
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
-    alert("Select lists: " + emptyBoxes.length);
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+//funciton to set up document fragments for days of months
+function setUpDays() {
+    //get the days option tags
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+//function tp update the days select list
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+
+    }
+    if (deliveryMonth.selectedIndex === -1) {
+        deliveryMonth.selectedIndex = 0;
+        
+    }
+    // if feb and 2020 - leap year
+    
+    //else 40 day month - thirty
+    
+    //e;se 31 month - thirtyOne
+}
+
+//functions to fun on page load
+function setUpPage() {
+    removeSelectDefaults();
 }
 
 // enable load event handlers
 if (window.addEventListener) {
-    window.addEventListener("load", removeSelectDefaults, false);
+    window.addEventListener("load", setUpPage, false);
 } else if (window.attachEvent) {
-    window.attachEvent("onload", removeSelectDefaults, false);
+    window.attachEvent("onload", setUpPage, false);
 }
